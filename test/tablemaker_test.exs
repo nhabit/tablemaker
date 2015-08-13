d5a263e4fe787f9b18e666d7ece50cdd4f4386c3
Andy Mendelsohn <amendelsohn@mac.com>
2015-08-13 10:59:11 +0100

    add 'context' to the first failing test

--- test/tablemaker_test.exs
+++ test/tablemaker_test.exs
@@ -1,19 +1,19 @@
 defmodule TablemakerTest do
 
-  use ExSpec, async: true
+    use ExSpec, async: true
 
-  def read_test_data do
-    {_,test_data} = File.read('test/test_data')
-    test_data
-  end
-
-  describe "running the command tablemaker" do
-      it "generates a multilne 10x10 multiplication grid of the first 10 prime numbers" do
-          {tabledata,_} = System.cmd("tablemaker", [])
-          assert tabledata == read_test_data
-      end
-  end
-
-
-
+    def read_test_data do
+        {_,test_data} = File.read('test/test_data')
+        test_data
+    end
+  
+    context "from the command line" do
+        describe "running the command tablemaker" do
+            it "generates a multi-line 10x10 multiplication grid of the first 10 prime numbers" do
+                {tabledata,_} = System.cmd("tablemaker", [])
+                assert tabledata == read_test_data
+            end
+        end
+    end
+    
 end
