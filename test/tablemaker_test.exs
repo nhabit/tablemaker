0876ce09233241c3f1251300e0359f0362d76c81
Andy Mendelsohn <amendelsohn@mac.com>
2015-08-13 11:19:04 +0100

    adding test for printing the table

--- test/tablemaker_test.exs
+++ test/tablemaker_test.exs
@@ -6,5 +6,12 @@ defmodule TablemakerTest do
         {_,test_data} = File.read('test/test_data')
         test_data
     end
-  
+
+    describe "print_table" do
+        it "generates a multi-line 10x10 multiplication grid of the first 10 prime numbers" do
+            tabledata = Tablemaker.print_table
+            assert tabledata == read_test_data
+        end
+    end
+
 end
