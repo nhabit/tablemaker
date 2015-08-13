963c7d514265488ee05d7b9d05cc3a2800b47ddb
Andy Mendelsohn <amendelsohn@mac.com>
2015-08-13 11:07:43 +0100

    Add an acceptance_test module and move code to rationalise it.

---
+++ test/acceptance_test.exs
@@ -0,0 +1,18 @@
+defmodule AcceptanceTest do
+
+    use ExSpec, async: true
+
+    def read_test_data do
+        {_,test_data} = File.read('test/test_data')
+        test_data
+    end
+  
+    context "from the command line" do
+        describe "running the command tablemaker" do
+            it "generates a multi-line 10x10 multiplication grid of the first 10 prime numbers" do
+                {tabledata,_} = System.cmd("tablemaker", [])
+                assert tabledata == read_test_data
+            end
+        end
+    end
+end
