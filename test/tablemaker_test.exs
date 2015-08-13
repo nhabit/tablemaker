1366f7f31bda28be1b6c1b2e3582165765db42bf
Andy Mendelsohn <amendelsohn@mac.com>
2015-08-13 11:09:29 +0100

    remove code from tablemaker_test.

--- test/tablemaker_test.exs
+++ test/tablemaker_test.exs
@@ -7,13 +7,4 @@ defmodule TablemakerTest do
         test_data
     end
   
-    context "from the command line" do
-        describe "running the command tablemaker" do
-            it "generates a multi-line 10x10 multiplication grid of the first 10 prime numbers" do
-                {tabledata,_} = System.cmd("tablemaker", [])
-                assert tabledata == read_test_data
-            end
-        end
-    end
-    
 end
