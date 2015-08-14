0aac26270ef76d346f00eadbd539de9a10a1dcb1
Andy Mendelsohn <amendelsohn@mac.com>
2015-08-14 23:11:25 +0100

    refactor print function and add create_data function

--- lib/tablemaker.ex
+++ lib/tablemaker.ex
@@ -2,6 +2,20 @@ defmodule Tablemaker do
 
     def main([]) do
       IO.puts("1")
+
     end
+
+    def print do
+        create_data 
+        #|>
+        #build_table |>
+        # print_table
+    end
+
+    def create_data() do 
+        count = 10
+        PrimeMaker.sieve_from(10)
+    end
+
 
 end
--- test/tablemaker_test.exs
+++ test/tablemaker_test.exs
@@ -7,10 +7,17 @@ defmodule TablemakerTest do
         test_data
     end
 
-    describe "print_table" do
-        it "generates a multi-line 10x10 multiplication grid of the first 10 prime numbers" do
-            tabledata = Tablemaker.print_table
+    describe "print" do
+        it "generates and outputs a multi-line multiplication grid of the first 10 prime numbers" do
+            tabledata = Tablemaker.print
             assert tabledata == read_test_data
+        end
+    end
+
+    describe "create_data" do
+        it "returns a list of the first 10 prime numbers" do
+            prime_list = Tablemaker.create_data
+            assert prime_list == [2,3,5,7,11,13,17,19.23,29]
         end
     end
 
