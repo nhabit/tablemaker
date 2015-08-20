9a9147124e5c561c9486d9f816369facc3cbd86e
Andy Mendelsohn <amendelsohn@mac.com>
2015-08-20 22:44:48 +0100

    remove hard-coded path. RED. tests still fail for calling args

--- test/acceptance_test.exs
+++ test/acceptance_test.exs
@@ -6,7 +6,7 @@ defmodule AcceptanceTest do
         
         describe "running the command tablemaker" do
             it "generates a multi-line 10x10 multiplication grid of the first 10 prime numbers" do
-                app_name = "/Users/andy/projects/elixir-stuff/tablemaker/tablemaker"
+                app_name = System.cwd() <> "/tablemaker"
                 {tabledata,_} = System.cmd(app_name, [])
                 assert tabledata == TestSetupHelper.read_test_data('test/test_data')
             end
@@ -14,7 +14,7 @@ defmodule AcceptanceTest do
     
         describe "running the command tablemaker with the --count option set to 15" do
             it "generates a multi-line 15x15 multiplication grid of the first 15 prime numbers" do
-                app_name = "/Users/andy/projects/elixir-stuff/tablemaker/tablemaker"
+                app_name = System.cwd() <> "/tablemaker"
                 {tabledata,_} = System.cmd(app_name, ["--count", "15"])
                 assert tabledata == TestSetupHelper.read_test_data('test/test_data_15_prime')
             end
@@ -22,7 +22,7 @@ defmodule AcceptanceTest do
 
         describe "running the command with the --type option set to fib" do
             it "generates a multi-line 10x10 multiplication grid of the first 10 fibonacci numbers" do
-                app_name = "/Users/andy/projects/elixir-stuff/tablemaker/tablemaker"
+                app_name = System.cwd() <> "/tablemaker"
                 {tabledata,_} = System.cmd(app_name, ["--type", "fib"])
                 assert tabledata == TestSetupHelper.read_test_data('test/test_data_10_fib')
             end
